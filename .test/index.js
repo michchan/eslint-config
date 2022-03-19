870f5c0851e49bb5755b62f19f1d8e683ed536d3
Michael Chan <michchandev@gmail.com>
2022-03-19 15:28:40 +0800

    build: upgrade node to 16 and use yarn

--- .test/index.js
+++ .test/index.js
@@ -1,15 +1,17 @@
 /**
  * Set and tag from version in package.json
  */
-const fs = require('fs')
-const { execSync } = require('child_process')
+const fs = require("fs");
+const { execSync } = require("child_process");
 
 // Get all config js files
 const configs = fs
-  .readdirSync('./')
-  .filter(filename => /(?<!index)\.js$/i.test(filename))
+  .readdirSync("./")
+  .filter((filename) => /(?<!index)\.js$/i.test(filename));
 
 // Test each config
-configs.forEach(name => execSync(`npm run eslint -- --config ./${name}`, {
-  stdio: 'inherit',
-}))
+configs.forEach((name) =>
+  execSync(`yarn eslint -- --config ./${name}`, {
+    stdio: "inherit",
+  })
+);
